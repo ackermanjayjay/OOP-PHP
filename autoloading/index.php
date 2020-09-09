3db769e69db36cae23e76ebbe98f88ee7453fe90
ackermanjayjay <reza23198@gmail.com>
2020-09-09 09:18:03 +0700

    Menambah fitur autoloading seperti framewrok keren lah pokoknya

---
+++ autoloading/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'App/init.php';
+// OBJECT DARI CLASS 
+$produk1 = new Komik("Naruto", "Shounen Jump", "Masashi Kishimoto", 30000, 100,);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 2500000, 50,);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+echo "<hr>";
+new Coba();
